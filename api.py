80e820d9f751efc71dde426ef549d202c926cd3a
Nathan Li Chau <95887491+Nlchau2004@users.noreply.github.com>
2023-08-14 20:44:02 +0000

    Created functions to scrape and parse holodex API

--- api.py
+++ api.py
@@ -1,8 +1,23 @@
 import requests
 import config
 
+
 headers = {"X-APIKEY": config.api_key}
 
-r = requests.get("https://holodex.net/api/v2/live?org=Hololive", headers=headers)
-for stream in r.json():
-    print(stream)
+
+def get_streams() -> dict:
+    holo_info = requests.get("https://holodex.net/api/v2/live?org=Hololive", headers=headers, timeout=2.5)
+    return holo_info.json()
+
+
+def parse(streams: dict, oshi: list) -> dict:
+    oshi_streams = {oshi:{} for oshi in oshi}
+    for stream in streams:
+        if stream["channel"]["english_name"] in oshi:
+            upcoming = {
+                "stream": stream["title"],
+                "scheduled_time": stream["start_scheduled"],
+                "link": stream["channel"]["id"]
+                }
+            oshi_streams[stream["channel"]["english_name"]].update(upcoming)
+    return oshi_streams
